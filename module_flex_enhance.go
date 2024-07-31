32511300ccb70879c0081686bc67a88a4d9733ed
CMA2401PT <1634268014@qq.com>
2024-07-31 14:40:16 +0800

    change signature to git rid of endless json.XX stuff

--- module_flex_enhance.go
+++ module_flex_enhance.go
@@ -5,22 +5,55 @@ import (
 	"github.com/OmineDev/neomega-core/utils/async_wrapper"
 )
 
+type CanSetArg interface {
+	WithArg(key string, arg any) CanSetArg
+	Launch()
+}
+
+type CanSetData interface {
+	WithJsonStrData(jsonStrData string)
+	WithJsonBytesData(jsonBytesData []byte)
+	WithJsonableAny(jsonableData any)
+	WithArg(key string, arg any) CanSetArg
+}
+
+type CanGetData interface {
+	RawJsonStr() string
+	RawJsonBytes() []byte
+	AsMap() (map[string]any, error)
+	Bind(target any) error
+	GetValue(key string) any
+	TakeValue(key string, value any) CanGetData
+}
+
+type CanSetArgThenResult interface {
+	WithArg(key string, arg any) CanSetArgThenResult
+	Launch() async_wrapper.AsyncResult[CanGetData]
+}
+
+type CanSetDataThenResult interface {
+	WithJsonStrData(jsonStrData string) async_wrapper.AsyncResult[CanGetData]
+	WithJsonBytesData(jsonBytesData []byte) async_wrapper.AsyncResult[CanGetData]
+	WithJsonableAny(jsonableData any) async_wrapper.AsyncResult[CanGetData]
+	WithArg(key string, arg any) CanSetArgThenResult
+}
+
 type FlexEnhance interface {
 	defines.FundamentalNode
 	defines.KVDataNode
 	defines.RolesNode
 	defines.TimeLockNode
 	// args should always be a json string
-	SoftCallOmitResult(cmd, args string)
+	SoftCallOmitResult(cmd string) CanSetData
 	// args should always be a json string
-	SoftCall(cmd, args string) async_wrapper.AsyncResult[string]
+	SoftCall(cmd string) CanSetDataThenResult
 	// e.g. if we what to add a new cmd ban <player> <reason> <time>
 	// we can register a new cmd like this:
 	// RegSoftAPI("ban", onCmd)
 	// where onCmd is a func(args string) output string
 	// and args is a string like "<player> <reason> <time>"
-	// args and ret should always be a json string
-	RegSoftAPI(cmd string) async_wrapper.AsyncAPISetHandler[string, string]
+	// args and ret should always be a json bytes
+	RegSoftAPI(cmd string) async_wrapper.AsyncAPISetHandler[CanGetData, []byte]
 
 	// e.g. if we want to add a new topic "player_banned"
 	// it can be used like this:
@@ -29,11 +62,11 @@ type FlexEnhance interface {
 	// bannedPlayer := SoftListen("player_banned")
 	// player1 := <-bannedPlayer
 	// player2 := <-bannedPlayer
-	SoftPublish(topic string, msg string)
-	SoftListen(topic string, nonBlockingMsgHandleFn func(string))
+	SoftPublish(topic string) CanSetData
+	SoftListen(topic string, nonBlockingMsgHandleFn func(CanGetData))
 
-	SoftGet(key string) (val string, found bool)
-	SoftSet(key string, val string)
+	SoftGet(key string) (val CanGetData, found bool)
+	SoftSet(key string) CanSetData
 
 	// cannot get/set in other process
 	InProcessGet(key string) (val any, found bool)
